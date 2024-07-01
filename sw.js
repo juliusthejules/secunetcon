03ca10274024520fb13d9441e0933e453dd163d0
Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>
2024-07-01 11:50:50 -0500

    Update sw.js
    Signed-off-by: Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const urlsToCache = [
-    './',
-    './index.html',
-    './styles.css',
-    './social.png',
-    './favicon.ico',
-    './icon-192x192.png',
-    './icon-256x256.png',
-    './icon-384x384.png',
-    './icon-512x512.png',
-    './app.webmanifest',
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/social.png',
+    '/favicon.ico',
+    '/icon-192x192.png',
+    '/icon-256x256.png',
+    '/icon-384x384.png',
+    '/icon-512x512.png',
+    '/app.webmanifest',
 ];
 
 // Install the service worker
